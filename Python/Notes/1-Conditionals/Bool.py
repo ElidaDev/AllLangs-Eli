df8beb58cdb6dff5e1604fa57a3aa9d28dff93d4
EliDev <Elijah6257@gmail.com>
2024-11-12 08:28:20 -0600

    Adding Conditional notes

---
+++ Python/Notes/1-Conditionals/Bool.py
@@ -0,0 +1,17 @@
+'''
+    Boolean Expressions and Operators
+
+    boolean - True or False
+    operator - math functions +,-,*,/
+
+    logic operators - 
+        <       less than
+        >       greater than
+        <=      less than or equal to
+        >=      greater than or equal to
+        ==      is it equal to
+        !=      is it not equal to
+        not     pythonic ! or opposite of the boolean exp to the right
+        and &&  both exp on both sides of the and is true
+        or ||   only one exp on either side of the or is true
+'''
