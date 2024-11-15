215f416da2fd802fac5e3341b50e10b5f8e5bce5
EliDev <Elijah6257@gmail.com>
2024-11-15 07:43:58 -0600

    Added notes and a BellRinger

---
+++ Python/BellRingers/StudentInfo.py
@@ -0,0 +1,14 @@
+studentinfo = []
+
+studentinfo.append(input("Enter your First Name: "))
+studentinfo.append(input("Enter your Last Name: "))
+studentinfo.append(input("Enter your HighSchool: "))
+studentinfo.append(input("Enter your Age: "))
+
+print(f'''
+Student Name: {studentinfo[0]} {studentinfo[1]}
+Student Email: {studentinfo[0]}.{studentinfo[1]}@stu.evsck12.com
+Student School: {studentinfo[2]}
+
+'''
+)
