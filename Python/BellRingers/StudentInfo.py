b8be3cb03dfb18edaf1a946d6c86c518e7e965ef
EliDev <Elijah6257@gmail.com>
2024-11-15 08:11:07 -0600

    Compressed StudentInfo Bellringer.

--- Python/BellRingers/StudentInfo.py
+++ Python/BellRingers/StudentInfo.py
@@ -1,14 +1,6 @@
 studentinfo = []
-
 studentinfo.append(input("Enter your First Name: "))
 studentinfo.append(input("Enter your Last Name: "))
 studentinfo.append(input("Enter your HighSchool: "))
 studentinfo.append(input("Enter your Age: "))
-
-print(f'''
-Student Name: {studentinfo[0]} {studentinfo[1]}
-Student Email: {studentinfo[0]}.{studentinfo[1]}@stu.evsck12.com
-Student School: {studentinfo[2]}
-
-'''
-)
+print(f'''\nStudent Name: {studentinfo[0]} {studentinfo[1]}\nStudent Email: {studentinfo[0]}.{studentinfo[1]}@stu.evsck12.com\nStudent School: {studentinfo[2]}\n''')
