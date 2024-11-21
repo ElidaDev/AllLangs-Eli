6d3b9cafc7596a2965fb29fd10d29adda3b780bb
EliDev <Elijah6257@gmail.com>
2024-11-21 07:47:31 -0600

    Added BellRinger and updated GAN in Bool.py

--- Python/Notes/1-Conditionals/Bool.py
+++ Python/Notes/1-Conditionals/Bool.py
@@ -48,26 +48,31 @@ print(bool("")) #False
 
 # IF it is not 0 then it is true
 # 
-
+import random
 
 guesses = 5
-secretNumber = 7    
-while True:
-    while True:
+secretNumber = random.randint(1,10)    
+loop = True
+x = 0
+while x != secretNumber:
+    while loop:
         try:
+            print(f"You have {guesses} guesses left.")
             x = int(input("Number: "))
-            break
+            loop = False
         except ValueError:
             print("Please insert an integer")
     if guesses >= 1:
         guesses -= 1
         if x > secretNumber:
             print("The number you guessed is bigger than the correct number!")
+            loop = True
         elif x < secretNumber:
             print("The number you guessed is less than the correct number!")
+            loop = True
         else:
             print("You got the number!")
     else:
         print("You ran out of guesses!")
-        break
+        x = secretNumber
 
