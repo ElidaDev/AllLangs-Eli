56567421860ee17b64ace09102fa7579b8beeaab
EliDaDev <Elijah6257@gmail.com>
2024-12-31 12:13:54 -0600

    Added SQRT to calculator.c

--- C/math.c
+++ C/math.c
@@ -25,6 +25,9 @@ double calculator(double x, double y, char operator){
         case '*':
             value = x*y;
             break;
+        case '^': //Sqrt
+            value = sqrt(x);
+            break;
         default:
             break;
     }
@@ -45,10 +48,12 @@ const char* isPrime(int num){
 }
 
 
+
 void main(){
     printf("%d\n", add(1,2));
     printf("%s\n", evenorodd(1));
     printf("%.16f\n", calculator(2.0,3.0,'a'));
+    printf("%.16f\n", calculator(4,0,'^')); 
     printf("%s\n", isPrime(1));
     printf("%s\n", isPrime(3));
     printf("%s\n", isPrime(4));
