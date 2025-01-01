467b9b1fea1d702322f2fd55e25406b066a948eb
EliDaDev <Elijah6257@gmail.com>
2025-01-01 17:05:34 -0600

    Updated tutorial, Learned Sorting arrays, and learned about structs and typedef...

--- C/tutorial.c
+++ C/tutorial.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <strings.h>
-
-int main(){
-    printf("Hello World!");
-    //How to write a comment
-
-    /*
-    Multi
-    Line
-    Comment
-    */
-
-    // Variables
-    int x; //Declaration
-    int y = 2; //Declaration/Initialization same line
-    char z = 'a';
-    char name[25];
-    double a = 1.5; // 
-    float b = 2.0;
-    char c[] = "This is an array of characters!";
-    long long d = 28638527;
-    // %c = char, %d = ints, %f = floats, %s for arrays of chars
-    printf("X = %d, z = %c, y = %d",x,z,y); // learned
-
+//Very important if you want to be efficient imo...
 /*
 Type          | Bytes | %formating |(Precision/min-max)              | Unsigned precision/min-max
 float         | 4     | %f         | 32 bit (6-7 digits)             |
@@ -41,14 +19,77 @@ Formatting
 %1
 %- (Left align)
 */
+struct Player
+{
+    char name[12];
+    int score;
+};
 
-// He talks about all the operators, but i know them all from other programming languages atleast what he went over...
+typedef char string[];
+
+int main(){
+    printf("Hello World!");
+    //How to write a comment
+
+    /*
+    Multi
+    Line
+    Comment
+    */
+
+    // // Variables
+    // int x; //Declaration
+    // int y = 2; //Declaration/Initialization same line
+    // char z = 'a';
+    // char name[25];
+    // double a = 1.5; // 
+    // float b = 2.0;
+    // char c[] = "This is an array of characters!";
+    // long long d = 28638527;
+    // double prices[10] = {1,2,3,4,5,6,7,8,9.5,10};
+    // // %c = char, %d = ints, %f = floats, %s for arrays of chars
+    // printf("X = %d, z = %c, y = %d",x,z,y); // learned
     // printf("Please input x: ");
     // scanf("%d", &x);
-    printf("What's your name: ");
-    fgets(name, 25, stdin);
-    name[strlen(name)-1] = '\0'; // gets rid of newline character
+    // printf("What's your name: ");
+    // fgets(name, 25, stdin);
+    // name[strlen(name)-1] = '\0'; // gets rid of newline character
+
+    // printf("You're name %s is pretty cool!", name);
+
+    // for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++){
+    //     printf("%lf\n", prices[i]);
+    // }
+    // int numbers[2][3];
+    // numbers[0][2] = 1; 
+    // char cars[][15] = {"Porsche", "Bugatti", "SRT", "Challenger"};
+    // strcpy(cars[0],"Corola");
+    // printf("%d\n", (sizeof(cars)/sizeof(cars[0])));
+    // for(int i = 0; i < (sizeof(cars)/sizeof(cars[0])); i++){
+    //     printf("%s\n", cars[i]);
+    // }
+
+    // char x[15] = "Water";
+    // char y[15] = "soda";
+    // char temp[15];
+
+    // strcpy(temp, x);
+    // strcpy(x, y);
+    // strcpy(y,temp);
+    // printf("\nX: %s\nY: %s",x,y);
+
+    //Structs
+
+    string name = "This is a string!";
+    printf("%s", name);
     
-    printf("You're name %s is pretty cool!", name);
+    
+    // struct Player player1;
+    // struct Player player2;
+    // strcpy(player1.name,"ME!");
+    // player1.score = 401;
+    // strcpy(player2.name,"NotMe...");
+    // player2.score = 1;
+    // printf("\n\n%-12s:%-4d\n%-12s:%-4d",player1.name,player1.score,player2.name,player2.score);
     return 0;
 }
