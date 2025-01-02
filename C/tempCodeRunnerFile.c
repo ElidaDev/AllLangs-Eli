5c8149197e37e1fbb02ade594de8e8e6cbcf6bbd
EliDaDev <Elijah6257@gmail.com>
2025-01-01 18:27:52 -0600

    Created Rock paper scissors

--- C/tempCodeRunnerFile.c
+++ C/tempCodeRunnerFile.c
@@ -1,12 +1,1 @@
-    // // Variables
-    // int x; //Declaration
-    // int y = 2; //Declaration/Initialization same line
-    // char z = 'a';
-    // char name[25];
-    // double a = 1.5; // 
-    // float b = 2.0;
-    // char c[] = "This is an array of characters!";
-    // long long d = 28638527;
-    // double prices[10] = {1,2,3,4,5,6,7,8,9.5,10};
-    // // %c = char, %d = ints, %f = floats, %s for arrays of chars
-    // printf("X = %d, z = %c, y = %d",x,z,y); // learned
+    char computerSelection = selection(time(0)); 
