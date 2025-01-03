00ebe447e2e619661f05ee2d32eb8d1cd0ca1ef2
EliDev <149635743+ElidaDev@users.noreply.github.com>
2025-01-02 22:25:52 -0600

    Update PrintingVariables.py

--- Python/2425/Notes/0-IO/PrintingVariables.py
+++ Python/2425/Notes/0-IO/PrintingVariables.py
@@ -48,3 +48,28 @@ print(story)
     \\t = tab
     \\  = \\ 
 '''
+
+def twoSum(nums, target):
+    for i in range(len (nums)){
+        for j in range(len (nums)){
+            if nums[i] + nums[j] == target:
+                return [i,j]
+            j+=1
+        }
+        i += 1
+    }
+
+nums = [2,7,11,15]
+target = 9
+#[0,1]
+print(twoSum(nums,target))
+
+nums = [3,2,4]
+target = 6
+#[1,2]
+print(twoSum(nums,target))
+
+nums = [3,3]
+target = 6
+#[0,1]
+print(twoSum(nums,target))
